ead6735724e6fdb1ef383dc6f032248e6f46ab15
Ian <iballard1995@gmail.com>
2021-09-18 12:00:17 -0400

    update conf

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+// const hre = require("hardhat");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,17 +23,53 @@ module.exports = {
     compilers: [
       {
         version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        }
       },
       {
         version: "0.8.4",
-        settings: {},
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
       },
     ],
     overrides: {
       "contracts/v6": {
         version: "0.6.12",
-        settings: {}
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        }
       }
     }
   },
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMAPI_KEY}`,
+        // url: "https://cloudflare-eth.com"
+
+        //Feel free to update at any time. This just make local development and caching easier
+        blockNumber: 13089820
+      },
+
+      // This is useful to imitate mainnet block processing
+      // mining: {
+      //     auto: false,
+      //     interval: 5000
+      // }
+    },
+    mainnet_cloudflare: {
+      url: "https://cloudflare-eth.com"
+    },
+  }
 };
