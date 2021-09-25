730970829d78bf563263272dd68194a4a9c2a804
Ian <iballard1995@gmail.com>
2021-09-25 12:24:52 -0400

    uni pool pt1

--- hardhat.config.js
+++ hardhat.config.js
@@ -42,10 +42,28 @@ module.exports = {
                     },
                 },
             },
+            {
+                version: "0.7.6",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1000,
+                    },
+                },
+            },
         ],
         overrides: {
             "contracts/v6": {
                 version: "0.6.12",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1000,
+                    },
+                }
+            },
+            "contracts/v7": {
+                version: "0.7.6",
                 settings: {
                     optimizer: {
                         enabled: true,
