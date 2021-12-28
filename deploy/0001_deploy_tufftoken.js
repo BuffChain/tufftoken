ca9ade267fd0315c87370ac30e60297c659a82cd
Jonathan Beckman <jonathantbeckman@gmail.com>
2021-12-28 18:08:16 -0500

    Make deployments idempotent

--- deploy/0001_deploy_tufftoken.js
+++ deploy/0001_deploy_tufftoken.js
@@ -28,25 +28,37 @@ module.exports = async () => {
     let tuffTokenDiamondContract = await hre.ethers.getContractAt(tuffTokenDiamond.abi, tuffTokenDiamond.address, contractOwner);
     console.log(`TuffTokenDiamond address [${await tuffTokenDiamondContract.address}]`);
 
-    let initTx = await tuffTokenDiamondContract.initTuffToken(contractOwner);
-    logDeploymentTx("Initialized TuffToken:", initTx);
+    if (!await tuffTokenDiamondContract.isTuffTokenInit()) {
+        let initTx = await tuffTokenDiamondContract.initTuffToken(contractOwner);
+        logDeploymentTx("Initialized TuffToken:", initTx);
+    }
 
-    initTx = await tuffTokenDiamondContract.initAaveLPManager(consts("AAVE_LENDINGPOOL_PROVIDER_ADDR"), [
-        consts("DAI_ADDR"), consts("USDC_ADDR"), consts("USDT_ADDR")
-    ]);
-    logDeploymentTx("Initialized AaveLPManager:", initTx);
+    if (!await tuffTokenDiamondContract.isAaveInit()) {
+        let initTx = await tuffTokenDiamondContract.initAaveLPManager(consts("AAVE_LENDINGPOOL_PROVIDER_ADDR"), [
+            consts("DAI_ADDR"), consts("USDC_ADDR"), consts("USDT_ADDR")
+        ]);
+        logDeploymentTx("Initialized AaveLPManager:", initTx);
+    }
 
-    initTx = await tuffTokenDiamondContract.initUniswapPriceConsumer(consts("WETH9_ADDR"), consts("DAI_ADDR"), UNISWAP_POOL_BASE_FEE, consts("UNISWAP_V3_FACTORY_ADDR"));
-    logDeploymentTx("Initialized UniswapPriceConsumer:", initTx);
+    if (!await tuffTokenDiamondContract.isUniswapPriceConsumerInit()) {
+        let initTx = await tuffTokenDiamondContract.initUniswapPriceConsumer(consts("WETH9_ADDR"), consts("DAI_ADDR"), UNISWAP_POOL_BASE_FEE, consts("UNISWAP_V3_FACTORY_ADDR"));
+        logDeploymentTx("Initialized UniswapPriceConsumer:", initTx);
+    }
 
-    initTx = await tuffTokenDiamondContract.initChainLinkPriceConsumer(consts("CHAINLINK_AGGREGATOR_ADDR"));
-    logDeploymentTx("Initialized ChainLinkPriceConsumer:", initTx);
+    if (!await tuffTokenDiamondContract.isChainLinkPriceConsumerInit()) {
+        let initTx = await tuffTokenDiamondContract.initChainLinkPriceConsumer(consts("CHAINLINK_AGGREGATOR_ADDR"));
+        logDeploymentTx("Initialized ChainLinkPriceConsumer:", initTx);
+    }
 
-    initTx = await tuffTokenDiamondContract.initMarketTrend(CHAINLINK_PRICE_CONSUMER_ENUM, false);
-    logDeploymentTx("Initialized MarketTrend:", initTx);
+    if (!await tuffTokenDiamondContract.isMarketTrendInit()) {
+        let initTx = await tuffTokenDiamondContract.initMarketTrend(CHAINLINK_PRICE_CONSUMER_ENUM, false);
+        logDeploymentTx("Initialized MarketTrend:", initTx);
+    }
 
-    initTx = await tuffTokenDiamondContract.initGovernance();
-    logDeploymentTx("Initialized Governance:", initTx);
+    if (!await tuffTokenDiamondContract.isGovernanceInit()) {
+        let initTx = await tuffTokenDiamondContract.initGovernance();
+        logDeploymentTx("Initialized Governance:", initTx);
+    }
 };
 
 module.exports.tags = ['v0001'];
