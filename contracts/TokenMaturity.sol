953e48762c46a10c8af3a5dadcf3fb9ee27be1fe
Ian Ballard <iballard1995@gmail.com>
2022-07-12 08:11:19 -0400

    token maturity docs

--- contracts/TokenMaturity.sol
+++ contracts/TokenMaturity.sol
@@ -64,41 +64,67 @@ contract TokenMaturity {
         return ss.isInit;
     }
 
+    /// @notice get the contracts maturity timestamp
+    /// @return contractMaturityTimestamp
     function getContractMaturityTimestamp() public view returns (uint256) {
         TokenMaturityLib.StateStorage storage ss = TokenMaturityLib.getState();
         return ss.contractMaturityTimestamp;
     }
 
+    /// @notice set the contracts maturity timestamp
+    /// @dev modifier onlyOwner can only be called by the contract itself or the contract owner
+    /// @param timestamp contract maturity timestamp
     function setContractMaturityTimestamp(uint256 timestamp) public onlyOwner {
         TokenMaturityLib.StateStorage storage ss = TokenMaturityLib.getState();
         ss.contractMaturityTimestamp = timestamp;
     }
 
+    /// @notice check if a timestamp is greater than or equal to contractMaturityTimestamp
+    /// @param timestamp timestamp to check against contract maturity timestamp
+    /// @return bool is matured
     function isTokenMatured(uint256 timestamp) public view returns (bool) {
         TokenMaturityLib.StateStorage storage ss = TokenMaturityLib.getState();
         return timestamp >= ss.contractMaturityTimestamp;
     }
 
+    /// @notice returns the total amount of VBT supply at the time of maturity
+    /// @return total VBT supply
     function totalSupplyForRedemption() public view returns (uint256) {
         TokenMaturityLib.StateStorage storage ss = TokenMaturityLib.getState();
         return ss.totalSupplyForRedemption;
     }
 
+    /// @notice set the total VBT supply upon token maturity
+    /// @dev modifier onlyOwner can only be called by the contract itself or the contract owner
+    /// @param _totalSupplyForRedemption total VBT supply
     function setTotalSupplyForRedemption(uint256 _totalSupplyForRedemption) public onlyOwner {
         TokenMaturityLib.StateStorage storage ss = TokenMaturityLib.getState();
         ss.totalSupplyForRedemption = _totalSupplyForRedemption;
     }
 
+    /// @notice gets the contract ETH balance at time of maturity
+    /// @return startingEthBalance
     function getContractStartingEthBalance() public view returns (uint256) {
         TokenMaturityLib.StateStorage storage ss = TokenMaturityLib.getState();
         return ss.startingEthBalance;
     }
 
+    /// @notice sets the contract ETH balance at time of maturity
+    /// @dev modifier onlyOwner can only be called by the contract itself or the contract owner
+    /// @param startingEthBalance eth balance at time of maturity
     function setContractStartingEthBalance(uint256 startingEthBalance) public onlyOwner {
         TokenMaturityLib.StateStorage storage ss = TokenMaturityLib.getState();
         ss.startingEthBalance = startingEthBalance;
     }
 
+    /**
+     * @notice calculates the redemption amount in ETH given a VBT balance.
+     *
+     * Redemption Amount = Contract ETH balance at time of maturity * amount of VBT held / total VBT supply
+     *
+     * @param ownerBalance amount of VBT to calculate redemption amount for
+     * @return redemption amount given VBT amount
+     */
     function getRedemptionAmount(uint256 ownerBalance) public view returns (uint256) {
         if (ownerBalance == 0) {
             return 0;
@@ -106,16 +132,37 @@ contract TokenMaturity {
         return getContractStartingEthBalance().mul(ownerBalance).div(totalSupplyForRedemption());
     }
 
+    /// @notice checks to see if VBT treasury has been fully liquidated
+    /// @return isTreasuryLiquidated
     function getIsTreasuryLiquidated() public view returns (bool) {
         TokenMaturityLib.StateStorage storage ss = TokenMaturityLib.getState();
         return ss.isTreasuryLiquidated;
     }
 
+    /// @notice sets isTreasuryLiquidated when treasury has been fully liquidated
+    /// @dev modifier onlyOwner can only be called by the contract itself or the contract owner
+    /// @param _isTreasuryLiquidated bool is treasury liquidated
     function setIsTreasuryLiquidated(bool _isTreasuryLiquidated) public onlyOwner {
         TokenMaturityLib.StateStorage storage ss = TokenMaturityLib.getState();
         ss.isTreasuryLiquidated = _isTreasuryLiquidated;
     }
 
+    /**
+     * @notice redemption function callable by VBT holder. Only redeemable once.
+     *
+     * calculates msg sender's VBT balance and redemption amount in ETH. Redemption amount is transferred to holder
+     * and the msg sender's VBT balance is then burned.
+     *
+     * Emits a {Redeemed} event.
+     *
+     * Requirements:
+     *
+     * - VBT must have reached maturity
+     * - VBT treasury must be fully liquidated
+     * - msg sender (holder) must have not already redeemed
+     * - msg sender (holder) must have a balance of VBT
+     *
+     */
     function redeem() public {
         //Redemption Before Expiration: Address can not redeem before expiration
         require(isTokenMatured(block.timestamp), "RBE");
@@ -148,20 +195,43 @@ contract TokenMaturity {
         emit Redeemed(account, ownerBalance, redemptionAmount);
     }
 
+    /// @notice checks to see if an address has already redeemed
+    /// @param account address to check if they have already redeemed
+    /// @return bool has already redeemed
+    /// @return amount redeemed
     function hasRedeemed(address account) public view returns (bool, uint256) {
         TokenMaturityLib.StateStorage storage ss = TokenMaturityLib.getState();
         return (ss.ownersRedeemed[account], ss.ownersRedemptionBalances[account]);
     }
 
+    /// @notice address balance of ETH
+    /// @return balance
     function balanceOfEth(address account) public view returns (uint256) {
         return account.balance;
     }
 
+    /// @notice gets contracts current ETH balance
+    /// @return balance
     function getCurrentContractEthBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    //    call via perform upkeep once current timestamp >= contract maturity timestamp
+    /**
+     * @notice called via perform upkeep once current timestamp >= contract maturity timestamp
+     *
+     * @dev modifier onlyOwner can only be called by the contract itself or the contract owner
+     *
+     * - Liquidates VBT Treasury
+     * - If treasury is fully liquidated, sets Starting Contract ETH balance to be used in redemption calculations
+     * - If treasury is fully liquidated, Sets VBT total redeemable supply
+     *
+     * Emits a {TokenMatured} event. (if treasury is fully liquidated)
+     *
+     * Requirements:
+     *
+     * - VBT must have reached maturity
+     *
+     */
     function onTokenMaturity() public onlyOwner {
         //Not Matured: TUFF - Token must have reached maturity
         require(isTokenMatured(block.timestamp), "NM");
@@ -181,6 +251,17 @@ contract TokenMaturity {
         emit TokenMatured(ethBalance, redeemableTotalSupply);
     }
 
+    /**
+     * @notice function responsible for liquidating the VBT treasury.
+     *
+     * @dev modifier onlyOwner can only be called by the contract itself or the contract owner
+     *
+     * - Liquidates Aave Treasury
+     * - For each VBT supported token, swap for WETH
+     * - Unwrap WETH to ETH
+     * - If all assets are liquidated, set isTreasuryLiquidated to true
+     *
+     */
     function liquidateTreasury() public onlyOwner {
         bool allAssetsLiquidated = true;
 
@@ -211,7 +292,11 @@ contract TokenMaturity {
         }
     }
 
-    //    swaps for WETH and returns new asset balance
+    /// @notice swaps for WETH and returns new asset balance. Uses Uniswap to execute the swap.
+    /// @dev modifier onlyOwner can only be called by the contract itself or the contract owner
+    /// @param token token to swap for WETH
+    /// @param amount amount of token to swamp
+    /// @return contract's new token balance
     function swapForWETH(address token, uint256 amount) public onlyOwner returns (uint256) {
         UniswapManagerLib.StateStorage storage ss = UniswapManagerLib.getState();
         UniswapManager uniswapManager = UniswapManager(address(this));
@@ -230,7 +315,9 @@ contract TokenMaturity {
         return erc20Token.balanceOf(address(this));
     }
 
-    //    unwraps WETH and returns remaining WETH balance
+    /// @notice unwraps WETH and returns remaining WETH balance.
+    /// @dev modifier onlyOwner can only be called by the contract itself or the contract owner
+    /// @return contract's WETH balance
     function unwrapWETH() public onlyOwner returns (uint256) {
         UniswapManagerLib.StateStorage storage ss = UniswapManagerLib.getState();
 
--- docs/TokenMaturity.md
+++ docs/TokenMaturity.md
@@ -87,10 +87,13 @@ function isTokenMaturityInit() public view returns (bool)
 function getContractMaturityTimestamp() public view returns (uint256)
 ```
 
+get the contracts maturity timestamp
 
 
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| [0] | uint256 | contractMaturityTimestamp |
 
 
 ### setContractMaturityTimestamp
@@ -99,9 +102,13 @@ function getContractMaturityTimestamp() public view returns (uint256)
 function setContractMaturityTimestamp(uint256 timestamp) public
 ```
 
+set the contracts maturity timestamp
 
+_modifier onlyOwner can only be called by the contract itself or the contract owner_
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| timestamp | uint256 | contract maturity timestamp |
 
 
 
@@ -111,10 +118,16 @@ function setContractMaturityTimestamp(uint256 timestamp) public
 function isTokenMatured(uint256 timestamp) public view returns (bool)
 ```
 
+check if a timestamp is greater than or equal to contractMaturityTimestamp
 
 
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| timestamp | uint256 | timestamp to check against contract maturity timestamp |
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| [0] | bool | bool is matured |
 
 
 ### totalSupplyForRedemption
@@ -123,10 +136,13 @@ function isTokenMatured(uint256 timestamp) public view returns (bool)
 function totalSupplyForRedemption() public view returns (uint256)
 ```
 
+returns the total amount of VBT supply at the time of maturity
 
 
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| [0] | uint256 | total VBT supply |
 
 
 ### setTotalSupplyForRedemption
@@ -135,9 +151,13 @@ function totalSupplyForRedemption() public view returns (uint256)
 function setTotalSupplyForRedemption(uint256 _totalSupplyForRedemption) public
 ```
 
+set the total VBT supply upon token maturity
 
+_modifier onlyOwner can only be called by the contract itself or the contract owner_
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| _totalSupplyForRedemption | uint256 | total VBT supply |
 
 
 
@@ -147,10 +167,13 @@ function setTotalSupplyForRedemption(uint256 _totalSupplyForRedemption) public
 function getContractStartingEthBalance() public view returns (uint256)
 ```
 
+gets the contract ETH balance at time of maturity
 
 
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| [0] | uint256 | startingEthBalance |
 
 
 ### setContractStartingEthBalance
@@ -159,9 +182,13 @@ function getContractStartingEthBalance() public view returns (uint256)
 function setContractStartingEthBalance(uint256 startingEthBalance) public
 ```
 
+sets the contract ETH balance at time of maturity
 
+_modifier onlyOwner can only be called by the contract itself or the contract owner_
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| startingEthBalance | uint256 | eth balance at time of maturity |
 
 
 
@@ -171,10 +198,18 @@ function setContractStartingEthBalance(uint256 startingEthBalance) public
 function getRedemptionAmount(uint256 ownerBalance) public view returns (uint256)
 ```
 
+calculates the redemption amount in ETH given a VBT balance.
+
+Redemption Amount = Contract ETH balance at time of maturity * amount of VBT held / total VBT supply
 
 
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| ownerBalance | uint256 | amount of VBT to calculate redemption amount for |
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| [0] | uint256 | redemption amount given VBT amount |
 
 
 ### getIsTreasuryLiquidated
@@ -183,10 +218,13 @@ function getRedemptionAmount(uint256 ownerBalance) public view returns (uint256)
 function getIsTreasuryLiquidated() public view returns (bool)
 ```
 
+checks to see if VBT treasury has been fully liquidated
 
 
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| [0] | bool | isTreasuryLiquidated |
 
 
 ### setIsTreasuryLiquidated
@@ -195,9 +233,13 @@ function getIsTreasuryLiquidated() public view returns (bool)
 function setIsTreasuryLiquidated(bool _isTreasuryLiquidated) public
 ```
 
+sets isTreasuryLiquidated when treasury has been fully liquidated
 
+_modifier onlyOwner can only be called by the contract itself or the contract owner_
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| _isTreasuryLiquidated | bool | bool is treasury liquidated |
 
 
 
@@ -207,7 +249,19 @@ function setIsTreasuryLiquidated(bool _isTreasuryLiquidated) public
 function redeem() public
 ```
 
+redemption function callable by VBT holder. Only redeemable once.
 
+calculates msg sender's VBT balance and redemption amount in ETH. Redemption amount is transferred to holder
+and the msg sender's VBT balance is then burned.
+
+Emits a {Redeemed} event.
+
+Requirements:
+
+- VBT must have reached maturity
+- VBT treasury must be fully liquidated
+- msg sender (holder) must have not already redeemed
+- msg sender (holder) must have a balance of VBT
 
 
 
@@ -219,10 +273,17 @@ function redeem() public
 function hasRedeemed(address account) public view returns (bool, uint256)
 ```
 
+checks to see if an address has already redeemed
 
 
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| account | address | address to check if they have already redeemed |
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| [0] | bool | bool has already redeemed |
+| [1] | uint256 | amount redeemed |
 
 
 ### balanceOfEth
@@ -231,10 +292,13 @@ function hasRedeemed(address account) public view returns (bool, uint256)
 function balanceOfEth(address account) public view returns (uint256)
 ```
 
+address balance of ETH
 
 
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| [0] | uint256 | balance |
 
 
 ### getCurrentContractEthBalance
@@ -243,10 +307,13 @@ function balanceOfEth(address account) public view returns (uint256)
 function getCurrentContractEthBalance() public view returns (uint256)
 ```
 
+gets contracts current ETH balance
 
 
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| [0] | uint256 | balance |
 
 
 ### onTokenMaturity
@@ -255,8 +322,19 @@ function getCurrentContractEthBalance() public view returns (uint256)
 function onTokenMaturity() public
 ```
 
+called via perform upkeep once current timestamp >= contract maturity timestamp
 
+_modifier onlyOwner can only be called by the contract itself or the contract owner
 
+- Liquidates VBT Treasury
+- If treasury is fully liquidated, sets Starting Contract ETH balance to be used in redemption calculations
+- If treasury is fully liquidated, Sets VBT total redeemable supply
+
+Emits a {TokenMatured} event. (if treasury is fully liquidated)
+
+Requirements:
+
+- VBT must have reached maturity_
 
 
 
@@ -267,8 +345,14 @@ function onTokenMaturity() public
 function liquidateTreasury() public
 ```
 
+function responsible for liquidating the VBT treasury.
 
+_modifier onlyOwner can only be called by the contract itself or the contract owner
 
+- Liquidates Aave Treasury
+- For each VBT supported token, swap for WETH
+- Unwrap WETH to ETH
+- If all assets are liquidated, set isTreasuryLiquidated to true_
 
 
 
@@ -279,10 +363,18 @@ function liquidateTreasury() public
 function swapForWETH(address token, uint256 amount) public returns (uint256)
 ```
 
+swaps for WETH and returns new asset balance. Uses Uniswap to execute the swap.
 
+_modifier onlyOwner can only be called by the contract itself or the contract owner_
 
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| token | address | token to swap for WETH |
+| amount | uint256 | amount of token to swamp |
 
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| [0] | uint256 | contract's new token balance |
 
 
 ### unwrapWETH
@@ -291,10 +383,14 @@ function swapForWETH(address token, uint256 amount) public returns (uint256)
 function unwrapWETH() public returns (uint256)
 ```
 
+unwraps WETH and returns remaining WETH balance.
+
+_modifier onlyOwner can only be called by the contract itself or the contract owner_
 
 
-
-
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| [0] | uint256 | contract's WETH balance |
 
 
 
